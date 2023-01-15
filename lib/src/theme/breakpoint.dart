8aeb0678fcb9c71dc64c22fea3aa3ca05e38f382
Ahmed Nouira <ahmnouira@gmail.com>
2023-01-15 21:08:41 +0100

    feat: added breakpoints

--- lib/src/theme/breakpoint.dart
+++ lib/src/theme/breakpoint.dart
@@ -1,16 +1,22 @@
 class Breakpoint {
   /// 8.0
-  static const x05 = 8.0;
+  static const xs = 8.0;
 
   /// 12.0
-  static const xs = 12.0;
-
-  /// 24.0
-  static const y2 = 24.0;
+  static const sm = 12.0;
 
   /// 16.0
   static const md = 16.0;
 
+  /// 20.0
+  static const lg = 20.0;
+
+  /// 24.0
+  static const xl = 24.0;
+
+  /// 28.0
+  static const xxl = 28.0;
+
   /// 32.0
-  static const x2 = 32.0;
+  static const xxxl = 32.0;
 }
