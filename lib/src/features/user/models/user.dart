a1984a4dea82fbc5c500d4507b3b45d7b2be3f76
Ahmed Nouria <ahmnouira@gmail.com>
2023-04-05 05:09:20 +0100

    feat: added preference service

---
+++ lib/src/features/user/models/user.dart
@@ -0,0 +1,6 @@
+class User {
+  final String id;
+  final String name;
+
+  const User({required this.id, required this.name});
+}
---
+++ lib/src/services/preference_service.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../features/user/models/user.dart';
+
+class PreferenceService {
+  Future<SharedPreferences> getInstance() async {
+    return SharedPreferences.getInstance();
+  }
+
+  Future<String?>? getItem(String key) async {
+    final instance = await getInstance();
+
+    return instance.getString(key);
+  }
+
+  Future<String?>? getUserId() async {
+    return getItem('id');
+  }
+
+  Future<bool> addItem(String key, String value) async {
+    final instance = await getInstance();
+
+    return instance.setString(key, value);
+  }
+
+  Future<bool> clear() async {
+    final instance = await getInstance();
+
+    return instance.clear();
+  }
+
+  Future<void> saveUser(User user, String token) async {
+    final pref = await getInstance();
+    pref.setString('id', user.id);
+    pref.setString('name', user.name);
+    pref.setString('token', token);
+  }
+}
