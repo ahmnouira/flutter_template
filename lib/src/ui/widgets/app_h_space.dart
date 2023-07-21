1b6c51e5383fdb5612dd83193bcbaa43a647e397
Ahmed Nouria <ahmnouira@gmail.com>
2023-07-21 15:48:25 +0100

    chore: upgrade deps

--- lib/src/ui/widgets/app_h_space.dart
+++ lib/src/ui/widgets/app_h_space.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class AppHSpace extends StatelessWidget {
   final double? width;
 
-  const AppHSpace({super.key, this.width = 16});
+  const AppHSpace({super.key, this.width = AppS});
+
+
+  const AppHSpace({super.key, this.width = AppS});
+
 
   @override
   Widget build(BuildContext context) {
